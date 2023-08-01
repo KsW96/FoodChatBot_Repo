29ea27c6de904cdf268228353ef1700e3475404f
GGG <GGG@DESKTOP-8N3GG4O>
2023-08-01 11:07:12 +0900

    한글 필터 생성

---
+++ src/util/CharacterEncodingFilter.java
@@ -0,0 +1,41 @@
+package util;
+
+import java.io.IOException;
+import java.net.HttpRetryException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CharacterEncodingFilter implements Filter {
+
+	private String encoding;
+	
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		// rq.setCharacterEncoding(encoding);
+		HttpServletResponse resp = (HttpServletResponse) res;
+		resp.setHeader("Content-Type", "text/plain;charset=" + encoding);
+		chain.doFilter(req, res);
+	}
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+		encoding = config.getInitParameter("encoding");
+		if (encoding == null) {
+			encoding = "UTF-8";
+		}
+		System.out.println(encoding);
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+}
