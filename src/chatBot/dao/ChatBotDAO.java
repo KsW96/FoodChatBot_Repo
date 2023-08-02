e9f9f441d5f91a9d056801dbea8cfbf32d312bf5
GGG <GGG@DESKTOP-8N3GG4O>
2023-08-02 11:51:15 +0900

    챗본 알고리즘 수정중..

--- src/chatBot/dao/ChatBotDAO.java
+++ src/chatBot/dao/ChatBotDAO.java
@@ -1,8 +1,52 @@
 package chatBot.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import util.DBUtil;
+
 public class ChatBotDAO {
 	// 키워드를 이용하여 매칭되는 음식명을 반환하는 메소드
 	public void selectByKeyword(String keyword) {
 		
+	}
+	
+	public int plusResolve() {
+		try (Connection conn = DBUtil.getConnection();
+				PreparedStatement stmt = conn.prepareStatement("UPDATE resolved SET count = count + 1 WHERE current_date();")) {
+			return stmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public String getResolve() {
+		try (Connection conn = DBUtil.getConnection();
+			PreparedStatement stmt = conn.prepareStatement("SELECT * FROM resolved WHERE date = current_date()")) {
+			try (ResultSet rs = stmt.executeQuery()) {
+				rs.next();
+				return rs.getString("count");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+
+	public String getById(int i) {
+		try (Connection conn = DBUtil.getConnection();
+			PreparedStatement stmt = conn.prepareStatement("SELECT * FROM food WHERE id = ?")) {
+			stmt.setInt(1, i);
+			try (ResultSet rs = stmt.executeQuery()) {
+				rs.next();
+				return rs.getString("name");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
 	}
 }
