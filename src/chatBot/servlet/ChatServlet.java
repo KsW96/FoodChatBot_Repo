eb4547e0c98ffd007ba39372675f750f3c016b8d
LeeSara4 <sarazzang05@gmail.com>
2023-08-04 10:00:38 +0900

    수정

--- src/chatBot/servlet/ChatServlet.java
+++ src/chatBot/servlet/ChatServlet.java
@@ -2,6 +2,8 @@ package chatBot.servlet;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import chatBot.dao.ChatBotDAO;
+import chatBot.service.recommendService;
 import imgFinder.ImageReturner;
 
 @WebServlet("/chat")
@@ -41,22 +44,33 @@ public class ChatServlet extends HttpServlet {
 		String chat = m.group(1);
 		System.out.println(chat);
 		
-		String food;
+		String chatbot;
+		chatbot = recommend(chat);
+		
 		if(chat.equals("사람") || chat.equals("날씨") || chat.equals("장소")) {
-			food = "그것은 어떤음식과 매칭?";
+			chatbot = "그것은 어떤음식과 매칭?";
 		} else if ( chat.equals("떡볶이") || chat.equals("돈가스") || chat.equals("죽") ) {
-			food = "감사합니다. 무엇을 먹고싶으세요?";
-		} else {
-			food = "그것은 무엇?";
+			chatbot = "감사합니다. 무엇을 먹고싶으세요?";
+		} else if (chat.equals("모르는단어")){
+			chatbot = "그것은 무엇?";
 		}
-		req.setAttribute("food", food);
-		System.out.println(food);
+		req.setAttribute("food", chatbot);
+		System.out.println(chatbot);
 		String resolve = "0";
 		
 		resp.setStatus(200);
 		resp.setHeader("Content-Type", "application/json;charset=utf-8");
-		resp.getWriter().write("{\"food\": \"" + food + "\",");
+		resp.getWriter().write("{\"food\": \"" + chatbot + "\",");
 		resp.getWriter().write("\"resolve\": \"" + resolve + "\"}");
+	}
+	
+	public String recommend(String chat) {
+		recommendService rs = new recommendService();
+		List<String> wordList = new ArrayList<String>();
+		System.out.println(rs.getFoodList());
+		wordList.add(chat);
+		String food = rs.recommendFood(wordList);
+		return food;
 	}
 
 
